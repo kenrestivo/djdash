4f742137a382929df090d95f1eb7a5e06b2e307e
ken restivo <ken@restivo.org>
2018-04-27 11:24:28 -0700

    Consolidate

--- user/user.clj
+++ user/user.clj
@@ -4,6 +4,7 @@
             [utilza.log :as ulog]
             [utilza.repl :as urepl]
             [clj-http.client :as client]
+            [figwheel-sidecar.repl-api :as ra]
             [cheshire.core :as json]
             [clojure.edn :as edn]
             [taoensso.timbre :as log]
@@ -12,10 +13,20 @@
 
 
 
+
+(defn start [] (ra/start-figwheel!))
+
+(defn stop [] (ra/stop-figwheel!))
+
+(defn cljs [] (ra/cljs-repl "dev"))
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (comment
 
-  
+
+  ;; XXX note live!!
   (ulog/catcher
    (let [p (->> "/mnt/sdcard/tmp/whynot.json"
                 slurp
@@ -26,5 +37,29 @@
                   {:form-params p
                    :content-type :json
                    :throw-exceptions true})))
+
+  (ulog/spewer
+   (client/post "http://localhost:8080/now-playing"
+                {:form-params {:foo "bar"}
+                 :content-type :json
+                 :as :json}))
+
+  
   )
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(comment
+
+  (start)
+
+  (stop)
+
+  
+
+  
+
+  )
